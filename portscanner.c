48e24e9c1bb9c6e817f036f6ce97ee94b78a7ed0
cffe3 <38264139+cffe3@users.noreply.github.com>
2018-05-07 06:35:57 -0400

    Added a single port syn scan option

--- portscanner.c
+++ portscanner.c
@@ -71,6 +71,133 @@ int syn_scan()
 		
 		}
 		
+	//close the socket each successful or 
+	//not (?) successful attempt.
+	close( serv_sock );
+	}
+		
+//once done
+//clean up
+//return 0 tells the OS that the program exited without errors
+
+printf( "[*]Done scanning \n" );
+printf( "[*]Cleaning up \n" );
+
+return 0;
+}
+
+int single_scan()
+{
+	
+	struct sockaddr_in s_sock;
+	
+	int serv_sock;
+	int port;
+	char ip_address[100];
+
+	//see if the initializing a socket works
+	//if not, throw a error and exit
+	if((serv_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		
+		error();
+		
+	return 1;
+	}
+	
+	s_sock.sin_family = AF_INET;
+
+	printf( "[*]The IP address: " );
+	scanf( "%s", &ip_address );
+	
+	s_sock.sin_addr.s_addr = inet_addr( ip_address );
+	
+	printf( "[*]The port number: " );
+	scanf( "%i", &port );
+	
+	s_sock.sin_port = htons( port );
+	
+	printf( "[*]Printing the result of the scan \n" );
+	
+	//if cannot connect
+	//assume the port is closed
+	if(connect(serv_sock, ( struct sockaddr * )&s_sock, sizeof( s_sock )) < 0)
+	{
+		
+		printf( "%i    closed \n", port );
+		
+	}
+	//else, assume it's open
+	else
+	{
+		
+		printf( "%i    open \n", port );
+		
+	}
+	
+	//clean up
+	//close socket
+	//and exit
+	//return 0 since this was a success
+	printf( "[*]Done scanning \n" );
+	printf( "[*]Cleaning up \n" );
+	close( serv_sock );
+	
+	return 0; //tells the OS that the program exited without errors
+}
+ 
+int main()
+{
+	
+	int input;
+	
+	printf( "[0] Exit \n" );
+	printf( "[1] Syn Scan \n" );
+	printf( "[2] Single Port Syn Scan \n" );
+	printf( "[*]Scan type: " );	
+	scanf( "%i", &input );
+	
+	//cannot equal 0
+	//since 0 is exit
+	if( input == 1 )
+	{
+			
+		syn_scan();
+			
+	}
+	//if equals 2,
+	//go straight to single port syn scan function
+	else if( input == 2 )
+	{
+		
+		single_scan();
+		
+	}
+	//if equals 0, 
+	//throw a error and exit
+	else if( input == 0 )
+	{
+			
+		error();
+	
+	return 1;
+	}
+	
+return 0; //tells the OS that the program exited without errors
+}
+
+		{
+			
+			printf( "%i    closed \n", port );
+			
+		}
+		else
+		{
+			
+			printf( "%i    open \n", port );
+		
+		}
+		
 	close( serv_sock );
 	}
 		
